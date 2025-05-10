3eeab97c928ac163e823978fab096c49a857069b
HoweyUMN <148487435+HoweyUMN@users.noreply.github.com>
2025-05-10 17:37:09 -0500

    Accidentally uploaded non-functional version

--- classification.py
+++ classification.py
@@ -1,7 +1,6 @@
 # %% 
 ### Imports
-import re
-import pandas as pd
+import re, pandas as pd
 import numpy as np
 import scipy as sp
 import tensorflow as tf
@@ -21,8 +20,8 @@ wandb.init()
 # %%
 ### Load the Data
 root = Path.cwd()
-sample_paths = [p for p in root.iterdir()
-                if p.is_dir() and (p/'matrix.mtx').exists()]
+sample_paths = [path for path in root.iterdir() # Get all the data that has these matrix files 
+                if path.is_dir() and (path/'matrix.mtx').exists()]
 
 X_list = []
 Y_list = []
@@ -31,9 +30,10 @@ gene_lists = []
 for path in tqdm(sample_paths, desc='Loading samples'):
     x = sp.io.mmread(path/'matrix.mtx').T.tocsr() # Provides raw count of each gene for given region
     genes = pd.read_csv(path/'features.tsv', sep='\t', header=None)[1].to_numpy()
-    region = re.search(r'sMMr([A-Z]{3})', path.name).group(1) # Our class labels
+    region = re.search(r'sMMr([A-Z]{3})', path.name).group(1) # Our class labels, AMY, ACA, and AUD
     labels = np.repeat(region, x.shape[0])
 
+    # update the lists
     X_list.append(x)
     Y_list.append(labels)
     gene_lists.append(genes)
@@ -54,6 +54,7 @@ for x_sample, genes_sample in zip(X_list, gene_lists):
     coo = x_sample.tocoo(copy=False)
     new_cols = local2glob[coo.col]
 
+    # Using sp.sparse seems to be MUCH faster
     reindexed = sp.sparse.csr_matrix(
         (coo.data, (coo.row, new_cols)),
         shape=(x_sample.shape[0], G), 
@@ -66,9 +67,9 @@ X = sp.sparse.vstack(aligned_X, format='csr')
 Y = np.concatenate(Y_list)
 genes = np.array(all_genes)
 
-print(f"Using {G:,} features in total")  
+print(f"Using {G} features in total")  
 
-# Limit to certain cell types using marker genes specific to these cell types
+# # Limit to certain cell types using marker genes specific to these cell types
 # marker_dict = {
 #     'Vip'    : ['Vip'],              
 #     'Pvalb'  : ['Pvalb'],            
@@ -77,32 +78,28 @@ print(f"Using {G:,} features in total")
 #     'Astro'  : ['Aqp4', 'Gfap']      
 # }
 
-# gene2idx = {g: i for i, g in enumerate(genes)} # Get a numeric key for each gene
+# gene2i = {g: i for i, g in enumerate(genes)} # Get a numeric key for each gene
 
 # # Build masks for each of the classes
 # class_masks = {}
 # for cname, markers in marker_dict.items():
 #     # Keep only markers that exist in all datasets to ensure cells are present in all regions
-#     idx = [gene2idx[m] for m in markers if m in gene2idx]
+#     idx = [gene2i[m] for m in markers if m in gene2i]
 #     if not idx:
-#         print(f"None of {markers} present after intersection; skipping {cname}") # Error checking (I believe this is obsolete now)
+#         print(f"None of {markers} present after intersection; skipping {cname}")
 #         continue
 #     expr = X[:, idx].toarray().sum(1).ravel()   
 #     class_masks[cname] = (expr > 0)
 
 # # Concatenate all the cell data into one master list
 # mask_matrix = np.column_stack(list(class_masks.values()))
-# # Cells with multiple expressions are just sorted into their first marker
+
+# # Cells with multiple expressions are just sorted into their highest marker
 # winner = mask_matrix.argmax(1)                    
-# keep = mask_matrix.sum(1) > 0                    
-# X, region = X[keep], Y[keep]
-# subclass= np.array(list(class_masks.keys()))[winner[keep]]
-
-# print("class counts:", pd.Series(subclass).value_counts().to_dict())
-
-# # Extract labels from brain region information 
-# Y = region 
-
+# keep   = mask_matrix.sum(1) > 0                    
+# X = X[keep]
+# Y = Y[keep]
+# subclass  = np.array(list(class_masks.keys()))[winner[keep]]
 
 #%%
 ### Extract the genes with the most variance to improve performance
@@ -110,7 +107,6 @@ gene_var = X.power(2).mean(0).A1 - np.square(X.mean(0).A1)
 top = np.argsort(gene_var)[-4000:]
 X = X[:, top]
 
-# Scale and normalize the dataset to get better feature extraction
 X = normalize(X, axis=1, norm='l1') * 1e4
 X = X.copy();  X.data = np.log1p(X.data)
 X = MaxAbsScaler(copy=False).fit_transform(X)
@@ -122,32 +118,34 @@ svd = TruncatedSVD(n_components=100, algorithm='randomized',
 X = svd.fit_transform(X)
 
 #%%
-# ### Load Presaved
-# data = np.load("svd_output/svd_features.npz")
-# X = data["X"]
-# Y = data["regions"]
+### Load Presaved
+data   = np.load("svd_output/svd_features.npz")
+X  = data["X"]
+Y = data["regions"]
 
 #%%
 ### Weight the classes
-class_weight = {i: len(Y) / np.sum(Y == lab) for i, lab in enumerate(np.unique(Y))}
+class_weight = {i: len(Y) / np.sum(Y == lab)
+                for i, lab in enumerate(np.unique(Y))}
 print("class weights:", class_weight)
 
 # %% 
 ### Preprocess for training with NN
 Y_enc = LabelEncoder().fit_transform(Y)
-x_train, x_test, y_train, y_test = train_test_split(X, Y_enc, test_size=0.5, random_state=42, stratify=Y_enc)
+x_train, x_test, y_train, y_test = train_test_split(
+        X, Y_enc, test_size=0.5, random_state=42, stratify=Y_enc)
 
 scaler = StandardScaler(with_mean=False)
-x_train = scaler.fit_transform(x_train)
+x_train = scaler.fit_transform(x_train) # Avoid poisoning test DS with training data
 x_test = scaler.transform(x_test)
 
 # %%
 ### Utilize a multilayer perceptron for classification 
 NDIM = x_train.shape[1]
 input = Input(shape=(NDIM,))
-hidden = Dense(2 * NDIM, activation='relu', kernel_initializer='he_normal')(input)
-hidden = Dropout(0.5)(hidden)
-hidden = Dense(NDIM, activation='relu', kernel_initializer='he_normal')(input)
+hidden = Dense(2 * NDIM, activation='relu', kernel_initializer='he_normal')(input) # initializer is supposed to help stabilize training
+hidden = Dropout(0.5)(hidden) # Overfitting was present, dropout helps reduce
+hidden = Dense(2* NDIM, activation='relu', kernel_initializer='he_normal')(input)
 hidden = Dropout(0.5)(hidden)
 hidden = Dense(NDIM, activation='relu', kernel_initializer='he_normal')(input)
 hidden = Dropout(0.5)(hidden)
@@ -160,7 +158,7 @@ hidden = Dropout(0.5)(hidden)
 output = Dense(len(np.unique(Y)), activation='softmax', kernel_initializer='he_normal')(hidden)
 
 model = Model(input, output)
-model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
+model.compile(optimizer=tf.keras.optimizers.Adam(1e-2),
               loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
@@ -170,16 +168,14 @@ def schedule(epoch, lr):
 history = model.fit(x_train, y_train, epochs=3, batch_size=16,
                     validation_split=0.5, verbose=1,
                     class_weight=class_weight,
-                    callbacks = [WandbMetricsLogger(), tf.keras.callbacks.LearningRateScheduler()])
+                    callbacks = [WandbMetricsLogger(), tf.keras.callbacks.LearningRateScheduler(schedule)])
 
 # %% 
 ### Evaluate the perceptron's performance
 preds = model.predict(x_test, batch_size=32)
 y_pred = np.argmax(preds, axis=1)
 print(classification_report(y_test, y_pred, target_names=np.unique(Y)))
-ConfusionMatrixDisplay.from_predictions(
-        y_test, y_pred, cmap='Blues', normalize='true',
-        display_labels=np.unique(Y))
+ConfusionMatrixDisplay.from_predictions(y_test, y_pred, cmap='Blues', normalize='true',display_labels=np.unique(Y))
 
 # %%
 ### Save preprocessed dataset
